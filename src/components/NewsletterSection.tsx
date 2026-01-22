a2264c98d6be8b5a47950da92a9f8a914dcfa6e8
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-01-22 12:34:06 -0300

    feat: adicionando responsividade para componente newsletter

--- src/components/NewsletterSection.tsx
+++ src/components/NewsletterSection.tsx
@@ -1,7 +1,7 @@
 export default function NewsletterSection() {
     return (
-        <div className="bg-[#e34926] p-1 tracking-wide">
-            <span className="text-white flex justify-center text-lg">
+        <div className="bg-[#e34926] p-2 tracking-wide">
+            <span className="text-white flex flex-col items-center text-center text-sm gap-1 sm:flex-row sm:justify-center sm:text-lg">
                 TOT News - A sua nova newsletter sobre Educação Corporativa! 
                 <a href="#"><u>Inscreva-se aqui.</u></a>
             </span>
